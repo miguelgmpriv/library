5a2e2f53ba2a75c0c9a475f031cbfcbd52e56c77
Miguel <miguelgm.priv@gmail.com>
2021-09-05 04:16:12 -0700

    Further define Book and addBook blocks Book constructor parameters set Rewrote event listeners and added a way to dismiss modal

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
-document.getElementById('new-book').addEventListener('click', function (e){
-    document.getElementById('new-modal').style.display = "block";
-});
 
 let myLibrary = []
-const bookForm = document.getElementById('book-form');
-bookForm.addEventListener('submit', addBook)
-function Book(){
+let bookNum = 0;
 
+const newBook = document.getElementById('new-book');
+const newModal = document.getElementById('new-modal');
+const bookForm = document.getElementById('book-form');
+
+newBook.addEventListener('click',() => newModal.style.display= 'block');
+bookForm.addEventListener('submit', addBook)
+window.onclick = function(e){
+    if (e.target == newModal) {newModal.style.display = 'none';}
+};
+
+function Book(){
+    this.name = bookForm.elements[0].value;
+    this.author = bookForm.elements[1].value;
+    this.pages = bookForm.elements[2].value;
+    this.read = bookForm.elements[3].value;
 }
 
 function addBook(event){
-    
-    console.log(event);
-    console.log(bookForm.elements[3].value);
+    myLibrary[bookNum] = new Book();
+    console.log(myLibrary[0]);
+    console.log(myLibrary[bookNum].name);
     event.preventDefault();
 }
