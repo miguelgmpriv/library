9490ef2c2fc04cc8b2747d4d309d86169cb63ec7
Miguel <miguelgm.priv@gmail.com>
2021-09-04 21:02:25 -0700

    Fix issue with modal form bug on mobile

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 document.getElementById('new-book').addEventListener('click', function (e){
     document.getElementById('new-modal').style.display = "block";
 });
+
+let myLibrary = []
+const bookForm = document.getElementById('book-form');
+bookForm.addEventListener('submit', addBook)
+function Book(){
+
+}
+
+function addBook(event){
+    
+    console.log(event);
+    console.log(bookForm.elements[3].value);
+    event.preventDefault();
+}
