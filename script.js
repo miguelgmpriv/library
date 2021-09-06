a917b24baba9e174eeac59bcec791d1df6dd2e3d
Miguel <miguelgm.priv@gmail.com>
2021-09-06 03:39:43 -0700

    Remove switch toggle from card Added button instead. Updated make card function.

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
 let myLibrary = []
 const original = document.querySelector(`div[data-book='']`);
-
+const library = document.getElementById('my-library')
 const newBook = document.getElementById('new-book');
 const newModal = document.getElementById('new-modal');
 const bookForm = document.getElementById('book-form');
@@ -22,38 +22,31 @@ function Book(){
 function addBook(event){
     myLibrary.push(new Book());
     makeCard(myLibrary.at(-1), (myLibrary.length - 1));
-
-    //console.log(clone.querySelector(`[data-title='']`).dataset)
-    //clone.dataset.title[bookNum].outerText = `${myLibrary[bookNum].title}`;
-    //console.log(clone.getElementsByTagName('h3')[0].outerText);
     event.preventDefault();
 }
 
 function makeCard(book, bookNum){
-    let clone = original.cloneNode(true);
-    bookItems = Object.keys(book);
+    //Clone original node and revise based on user input. Updates data attributes as well
+    let clone = original.cloneNode(true); 
+    bookItems = Object.keys(book); //Turn object into array so we can use in for loop
     clone.dataset.book = `${bookNum}`;
     for (counter = 0; counter < 4; counter++){
-        if (counter === 3){
-            clone.children[counter].children[0].dataset[bookItems[counter]] = `${bookNum}`
-            clone.children[counter].children[0].checked = book[bookItems[counter]]
-            continue;
-        }
+        const displayText = clone.children[counter].textContent;
         clone.children[counter].dataset[bookItems[counter]] = `${bookNum}`
-        clone.children[counter].textContent = book[bookItems[counter]];
+        if (counter === 3){
+            if (book[bookItems[counter]]){ 
+                clone.children[counter].innerText = 'Read';
+            } else {
+                clone.children[counter].innerText = 'Not Read';
+            }
+        continue;
+        }
+        clone.children[counter].textContent += book[bookItems[counter]];
     }
-
-/*     clone.children[0].dataset = `${bookNum}`;
-    clone.children[1].dataset = `${bookNum}`;
-    clone.children[2].dataset = `${bookNum}`;
-
-
-    clone.children[3].dataset.read = `${bookNum}`;
-    clone.children[0].textContent = book.title;
-    clone.children[1].textContent = book.author;
-    clone.children[2].textContent = book.pages;
-    clone.children[3].children[0].checked = book.read; */
-    
-
-    original.after(clone);
+    if (bookNum === 0){
+        original.remove();
+        library.appendChild(clone);
+        return;
+    }
+    library.appendChild(clone);
 }
