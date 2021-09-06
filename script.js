990594aa5b907bdeb6fc087782adc77057aa1acb
Miguel <miguelgm.priv@gmail.com>
2021-09-06 10:57:44 -0700

    Add toggleRead function toggleRead changes myLibrary.read value and background color on click. Revised makeCard function to display correct background on button.

--- script.js
+++ script.js
@@ -24,19 +24,33 @@ function Book(){
 function routeClick(event){
     const dataAtt = event.target.dataset
     if(dataAtt.remove) removeCard(event);
+    if(dataAtt.read) toggleRead(event);
 }
 
 function addBook(event){
     myLibrary.push(new Book());
     makeCard(myLibrary.at(-1), (myLibrary.length - 1));
     event.preventDefault();
+}
 
+function toggleRead(event){
+    let bookRead = event.target
+    if (bookRead.textContent === 'Read'){
+        bookRead.textContent = 'Not Read';
+        myLibrary[bookRead.dataset.read].read = false;
+        bookRead.style.backgroundColor = 'rgb(133, 8, 8)';
+    } else {
+        bookRead.textContent = 'Read';
+        myLibrary[bookRead.dataset.read].read = true;
+        bookRead.style.backgroundColor = 'blue';
+    }
 }
 
 function makeCard(book, bookNum){
     //Clone original node and revise based on user input. Updates data attributes as well
     let clone = original.cloneNode(true); 
     bookItems = Object.keys(book); //Turn object into array so we can use in for loop
+    //Add dataset values to children. We likely only needed one for book though
     clone.dataset.book = `${bookNum}`;
     clone.children[4].dataset.remove = `${bookNum}`;
     for (counter = 0; counter < 4; counter++){
@@ -44,6 +58,7 @@ function makeCard(book, bookNum){
         if (counter === 3){
             if (book[bookItems[counter]]){ 
                 clone.children[counter].innerText = 'Read';
+                clone.children[counter].style.backgroundColor = 'blue';
             } else {
                 clone.children[counter].innerText = 'Not Read';
             }
@@ -51,17 +66,19 @@ function makeCard(book, bookNum){
         }
         clone.children[counter].textContent += book[bookItems[counter]];
     }
+    //Removes original if first time
     original.remove();
     clone.style.display = 'block';
     library.insertBefore(clone, newBook);
 }
 
 function removeCard(event){
-    const delCard = event.target.parentNode;
-    const indexDel = delCard.dataset.book;
+    //Deletes all divs except add-book
+    const indexDel = event.target.parentNode.dataset.book;
     while (library.firstChild.id !== 'add-book'){
         library.removeChild(library.firstChild);
     }
+    //Removes from the library array based on the dataset value, then redraw library
     myLibrary.splice(indexDel, 1);    
     for (let counter = 0, max = myLibrary.length - 1; counter <= max; counter++){
         makeCard(myLibrary[counter],counter);
